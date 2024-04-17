bc0f33951cce853631f7b93072d42dabc51d0a4a
CarmineStarita <163003568+CarmineStarita@users.noreply.github.com>
2024-04-17 16:03:40 +0200

    Base SpringbootBE Top5
    Base BE Top5

---
+++ Top5BE/src/main/java/it/unife/BEtop5/Top5BE/Top5BeApplication.java
@@ -0,0 +1,13 @@
+package it.unife.BEtop5.Top5BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Top5BeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Top5BeApplication.class, args);
+	}
+
+}
---
+++ Top5BE/src/test/java/it/unife/BEtop5/Top5BE/Top5BeApplicationTests.java
@@ -0,0 +1,13 @@
+package it.unife.BEtop5.Top5BE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Top5BeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
