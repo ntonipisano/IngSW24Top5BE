bd1a463a4050342a88e26b3627549e6f1e4c3c1c
ntonipisano <antoniofederi.pisano@edu.unife.it>
2024-04-20 14:36:36 +0200

    Create IngSW24Top5BEApplication.java
    avevo sbagliato a eliminare questo file creato da springboot

---
+++ Top5BE/src/main/java/it/unife/BEtop5/Top5BE/IngSW24Top5BEApplication.java
@@ -0,0 +1,13 @@
+package it.unife.BEtop5.Top5BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngSW24Top5BEApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IngSW24Top5BEApplication.class, args);
+    }
+
+}
