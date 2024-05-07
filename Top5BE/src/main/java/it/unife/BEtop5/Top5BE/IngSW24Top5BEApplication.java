062e7549e105b8db5753c87e55e84c9d2e807514
ntonipisano <antoniofederi.pisano@edu.unife.it>
2024-05-08 01:09:16 +0200

    PROGRAMMA FUNZIONANTE
    stampa i bean utilizzati

--- Top5BE/src/main/java/it/unife/BEtop5/Top5BE/IngSW24Top5BEApplication.java
+++ Top5BE/src/main/java/it/unife/BEtop5/Top5BE/IngSW24Top5BEApplication.java
@@ -1,5 +1,11 @@
 package it.unife.BEtop5.Top5BE;
 
+import java.util.Arrays;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,5 +15,18 @@ public class IngSW24Top5BEApplication {
     public static void main(String[] args) {
         SpringApplication.run(IngSW24Top5BEApplication.class, args);
     }
+    @Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+        return args -> {
 
+            System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+            String[] beanNames = ctx.getBeanDefinitionNames();
+            Arrays.sort(beanNames);
+            for (String beanName : beanNames) {
+                System.out.println(beanName);
+            }
+
+        };
+    }
 }
