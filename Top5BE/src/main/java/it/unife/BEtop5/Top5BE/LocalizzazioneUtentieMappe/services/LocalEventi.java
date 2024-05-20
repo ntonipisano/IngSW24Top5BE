d0e86f23c847c7892019a253183574742e704943
ntonipisano <antoniofederi.pisano@edu.unife.it>
2024-05-20 10:58:59 +0200

    controller e servizio

--- Top5BE/src/main/java/it/unife/BEtop5/Top5BE/LocalizzazioneUtentieMappe/services/LocalEventi.java
+++ Top5BE/src/main/java/it/unife/BEtop5/Top5BE/LocalizzazioneUtentieMappe/services/LocalEventi.java
@@ -4,13 +4,20 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.client.RestClient;
 @SpringBootApplication
 public class LocalEventi {
-    static String uriBaseMock = "http://localhost:3001/posizionevento";  //URL endpoint Mockoon
 
-    public static String callREST(String uriBaseMock) {
+    static String uriBaseMock = "http://localhost:3001/posizionevento";
+    public static String callREST(String resourceName, String uriBase, boolean useMock) {
         RestClient restClient = RestClient.create();
+        /*
+        Creo uriBase per chiamare Mockoon se l'impostazione è useMock
+         */
+        if(useMock)
+            uriBase = uriBaseMock;
+
+        System.out.println(uriBase+resourceName);
 
         return restClient.get()
-                .uri(uriBaseMock)
+                .uri(uriBase + resourceName)
                 .retrieve()
                 .body(String.class);
     }
